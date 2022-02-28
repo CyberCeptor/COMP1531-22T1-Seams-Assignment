e4d10abd6ca72b8610b44b375a8dd83d8e0197b0
z5360181 <z5360181@student.unsw.edu.au>
2022-02-28 22:39:44 +1100

    finialising test_channels and channels.py

--- src/channels.py
+++ src/channels.py
@@ -1,4 +1,23 @@
+from src.error import InputError
+from src.error import AccessError
+from src.auth import auth
+
+from data_store import data_store
+
+
+
+
+
 def channels_list_v1(auth_user_id):
+
+    # looping threough data_store['channels']
+    # search through 'all_members', when the auth_user_id occurs,
+    # add the channel information to the new dict.
+    # return new dict.
+
+
+
+
     return {
         'channels': [
         	{
@@ -7,6 +26,12 @@ def channels_list_v1(auth_user_id):
         	}
         ],
     }
+
+
+
+
+
+
 
 def channels_listall_v1(auth_user_id):
     return {
@@ -18,25 +43,69 @@ def channels_listall_v1(auth_user_id):
         ],
     }
 
+
+
+
+    # Creates a new channel with the given name and is either public or private.
+    # The user who created it automatically joins it.
+    # Returns the channel id.
+
 def channels_create_v1(auth_user_id, name, is_public):
-    '''
-Parameters: 
-    {auth_user_id,  (int)
-    name,           (string)
-    is_public}      (Boolean, True/False)
+    # retrieving channel data from data_store
+    store = data_store.get()
 
-Return Type:
-    {channel_id}    (int)
+    if auth_user_id < 1:
+        raise AccessError("Error: the user id is not valid (out of bounds).")
 
-Exceptions:
-    InputError when: length of name is less than 1 or more than 20 characters
+    if len(name) > 20:
+        raise InputError("Error: the channel name must be less than 20 characters.")
 
-'''
+    if len(name) < 1:
+        raise InputError("Error: no channel name was entered.")
+
+    if type(is_public) != bool:
+        raise InputError("Error: the public/private value given is not of type bool.")
+
+
+    # Test channel names for repition, unless public vs private.
+    # Loops through data_store['channels'] to check channel names if they already exist
+    # and are of the same is_public (public/private) then cannot be created.
+    # Having two channles with the same name is fine, as long as they have different is_public values.
+    for channel in store['channels']:
+        if channel['name'] == name and channel['is_public'] == is_public:
+            raise InputError("This channel name already exists.")
+
+    # Check that the auth_user_id exists.
+    user_exists = False
+    for user in store['users']:
+        if user['id'] == auth_user_id:
+            user_exists = True
+    # if the auth_user_id is not found, the user does not exist.
+    if user_exists == False:
+        raise AccessError("User does not exist in users database.")
+
+
+    #Need to check that the auth_user_id is valid by comparing from the data dict.
+
+
+
+
+
+    # get the number of channels created so far, incremented for the new channel id.
+    channel_id = len(data_store['channels']) + 1
+
+    # Storing the channel information 
+    channel_data = {
+        'channel_id': channel_id,
+        'name': name,
+        'owner_members': [auth_user_id],
+        'all_members': [auth_user_id],
+        'is_public': is_public,
+    }
+
+    store['channels'].append(channel_data)
     
-    
-    
-    
-    
+
     return {
-        'channel_id': 1,
+        'channel_id': channel_id
     }
--- tests/channels_test.py
+++ tests/channels_test.py
@@ -8,6 +8,8 @@ from src.error import AccessError
 
 
 
+# Assumption that valid login from auth_register.
+
 #####################################################
 #                                                   #
 #          Channels Create Test Functions           #
@@ -25,52 +27,64 @@ Return Type:
 
 ### auth['auth_user_id'] is accessing the item in the Dictionary of User.
 ### is_public is True and False, so need to test for both public and private channels.
+@pytest.fixture
+def clear_and_register():
+    clear_v1()
+    auth_register_v1('abc@def.com', 'password', 'first', 'last')
 
 # Testing create only works with valid auth_user_id. Access Error
 def test_channels_create_valid_auth_id():
-    clear_v1()
     with pytest.raises(AccessError):
-        assert channels_create_v1('-4', 'test_channel_public', True)
-        assert channels_create_v1('-4', 'test_channel_private', False)
+        channels_create_v1(2, 'test_channel_public', True)
+    with pytest.raises(AccessError):
+        channels_create_v1(2, 'test_channel_private', False)
+    with pytest.raises(AccessError):
+        channels_create_v1(-2, 'test_channel_public2', True)
+    with pytest.raises(AccessError):
+        channels_create_v1(-2, 'test_channel_private2', False)
 
 # Testing channel name is greater then 1 character, gives no input for name. Input Error
 def test_channels_create_too_short():
-    clear_v1()
-    #return auth_user_id
-    auth = auth_register_v1('abc@def.com', 'password', 'first', 'last')
-    assert channels_create_v1(auth['auth_user_id'], "", True)
-    assert channels_create_v1(auth['auth_user_id'], "", False)
+    with pytest.raises(InputError):
+        channels_create_v1(1, "", True)
+    with pytest.raises(InputError):
+        channels_create_v1(1, "", False)
 
 # Testing for channel name longer than 20 characters for both public and private channels. InputError.
-def test_channels_create_channel_name_too_long():
-    clear_v1()
-    auth = auth_register_v1('abc@def.com', 'password', 'first', 'last')
+def test_channels_create_invalid_name():
     with pytest.raises(InputError):
-        assert channels_create_v1(auth['auth_user_id'], 'MoreThan20CharPublic!', True)
-        assert channels_create_v1(auth['auth_user_id'], 'MoreThan20CharPrivate', False)
+        channels_create_v1(1, 'MoreThan20CharPublic!', True)
+    with pytest.raises(InputError):
+        channels_create_v1(1, 'MoreThan20CharPrivate', False)
 
 # Testing that the is_public argument is a boolean. Input Error
 def test_channels_create_boolean():
-    clear_v1()
-    auth = auth_register_v1('abc@def.com', 'password', 'first', 'last')
     with pytest.raises(InputError):
-        assert channels_create_v1(auth['auth_user_id'], 'test_channel', 'Not a boolean')
+        channels_create_v1(1, 'test_channel', 'Not a boolean')
 
-# Source code https://pythonguides.com/python-check-if-the-variable-is-an-integer/
+# Testing duplicate channels names created with the same is_public.
+def test_channels_duplicate_name():
+    channels_create_v1(1, 'test_channel_public3', True)
+    with pytest.raises(InputError):  
+        channels_create_v1(1, 'test_channel_public3', True)
+
+    channels_create_v1(1, 'test_channel_private4', False)
+    with pytest.raises(InputError):  
+        channels_create_v1(1, 'test_channel_private4', False)
+
+
 # Testing the return value of channels_create is a valid int for both public and private. 
-def test_channels_create_return():
-    clear_v1()
-    auth = auth_register_v1('abc@def.com', 'password', 'first', 'last')
-    channel_id_one = channels_create_v1(auth['auth_user_id'], 'test_channel_public', True)
-    channel_id_two = channels_create_v1(auth['auth_user_id'], 'test_channel_private', False)
-    with pytest.raises(AccessError):
-        assert isinstance(channel_id_one['channel_id'], int)
-        assert isinstance(channel_id_two['channel_id'], int)
-
+def test_channels_create_return(clear_and_register):
+    channel_id_one = channels_create_v1(1, 'test_channel_public', True)
+    channel_id_two = channels_create_v1(1, 'test_channel_private', False)
+    assert channel_id_one['channel_id'] == 1
+    assert channel_id_two['channel_id'] == 2
 
 
 #####################################################
-#       Channels List Test Functions                #
+#                                                   #
+#           Channels List Test Functions            #
+#                                                   #
 #####################################################
 
 
