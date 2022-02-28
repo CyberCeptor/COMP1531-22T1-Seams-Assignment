e72369b4c03e18396219afe8be5b23cb510b4a69
Aleesha <z5371516@ad.unsw.edu.au>
2022-02-28 15:54:07 +1100

    edited test_clear_works

--- tests/other_test.py
+++ tests/other_test.py
@@ -8,6 +8,6 @@ from src.data_store import data_store
 def test_clear_works():
     store = data_store.get()
     auth_register_v1('abc@def.com', 'password', 'first', 'last')
+    assert store['users'] != []
     clear_v1()
     assert store['users'] == []
-    
