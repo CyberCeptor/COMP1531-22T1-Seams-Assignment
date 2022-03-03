7ed8e9d85d8e1a90a419a06c5bf6cec249c2d477
Aleesha <z5371516@ad.unsw.edu.au>
2022-03-03 23:47:37 +1100

    added global_owners to channel_data

--- src/channels.py
+++ src/channels.py
@@ -81,6 +81,7 @@ def channels_create_v1(auth_user_id, name, is_public):
         'name': name,
         'owner_members': [auth_user_id],
         'all_members': [auth_user_id],
+        'global_owners': [auth_user_id],
         'is_public': is_public,
     }
 
