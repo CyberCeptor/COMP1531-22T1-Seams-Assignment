379f3aa406aca7ca5a082f18326e6ac5e89a0011
Aleesha <z5371516@ad.unsw.edu.au>
2022-03-04 21:36:42 +1100

    deleted data_store import

--- tests/channel_test.py
+++ tests/channel_test.py
@@ -6,7 +6,6 @@ from src.error import InputError
 from src.error import AccessError
 from src.channels import channels_create_v1
 from src.channel import channel_invite_v1
-from src.data_store import data_store
 from src.channel import channel_join_v1
 
 #### Test Chanel_invite    Zefan Cao(Van) z5237177
