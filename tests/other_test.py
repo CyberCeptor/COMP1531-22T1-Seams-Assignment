9a2f2973115bfb00b3e16896fca4d4a8d1749e81
Aleesha <z5371516@ad.unsw.edu.au>
2022-03-04 20:40:12 +1100

    fixed up style for other.py and other_test.py

--- tests/other_test.py
+++ tests/other_test.py
@@ -1,12 +1,26 @@
-import pytest
+"""
+Filename: other_test.py
+
+Author: group
+Created: 24/02/2022 - 04/03/2022
+
+Description: pytest for checking if clear_v1() works
+"""
 
 from src.auth import auth_register_v1
 from src.other import clear_v1
-
-from src.data_store import data_store
 from src.channels import channels_create_v1
+from src.data_store import data_store
 
 def test_clear_works():
+    """ checks if all stored data is gone once clear_v1 is called
+
+    Arguments: N/A
+
+    Exceptions: N/A
+
+    Return Value: N/A """
+
     store = data_store.get()
     auth_register_v1('abc@defg.com', 'password', 'first', 'last')
     channels_create_v1(1, 'test_channel', True)
@@ -15,4 +29,3 @@ def test_clear_works():
     clear_v1()
     assert store['users'] == []
     assert store['channels'] == []
-
