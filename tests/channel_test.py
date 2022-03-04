98adf1eaf8255cb7d0745776e176af96294d14ec
Zefan Cao <Z5237177@unsw.edu.au>
2022-03-04 15:25:06 +1100

    check

--- tests/channel_test.py
+++ tests/channel_test.py
@@ -65,3 +65,42 @@ def test_channel_invite_unauthorised():
     inviter_info = auth_register_v1('wangkaiyan233@gmail.com', 'wky19991123', 'Wang', 'kaiyan')
     with pytest.raises(AccessError):
         channel_invite_v1(inviter_info['auth_user_id'], createchannel['channel_id'], invitee_info['auth_user_id'])
+
+######## Test channel_join_v1 Zefan Cao z5237177
+#
+#
+#
+# Test successful
+def test_channel_join_successful():
+    clear_v1()
+    store = data_store.get()
+    inviter_info = auth_register_v1('wangkaiyan233@gmail.com', 'wky19991123', 'Wang', 'kaiyan')
+    invitee_info = auth_register_v1('xuezhiqian234@gmail.com', 'xzq19991123', 'Xue', 'zhiqian')
+    newchannel = channels_create_v1(inviter_info['auth_user_id'], 'namechannelwky', True)
+    channel_join_v1(invitee_info['auth_user_id'], newchannel['channel_id'])
+    assert len(store['channels'][0]['all_members']) == 2
+
+
+# InputError:Channel is an invalid channel
+def test_join_invalid_channel():
+    clear_v1()
+    invitee_info = auth_register_v1('xuezhiqian234@gmail.com', 'xzq19991123', 'Xue', 'zhiqian')
+    with pytest.raises(InputError):
+        channel_join_v1(1, 0)
+
+# Inputerror: user is already in channel
+def test_join_already_exist():
+    clear_v1()
+    inviter_info = auth_register_v1('wangkaiyan233@gmail.com', 'wky19991123', 'Wang', 'kaiyan')
+    channels_create_v1(1, 'validchannelname', True)
+    with pytest.raises(InputError):
+        channel_join_v1(1, 1)
+
+# AccessError: channel is valid that is private and the user is not a global owner
+def test_join_channel_is_private():
+    clear_v1()
+    inviter_info = auth_register_v1('wangkaiyan233@gmail.com', 'wky19991123', 'Wang', 'kaiyan')
+    invitee_info = auth_register_v1('xuezhiqian234@gmail.com', 'xzq19991123', 'Xue', 'zhiqian')
+    newchannel = channels_create_v1(inviter_info['auth_user_id'], 'validchannelname', False) # it is a private channel
+    with pytest.raises(AccessError):
+        channel_join_v1(invitee_info['auth_user_id'], newchannel['channel_id'])
