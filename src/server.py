d735001defd05414620b438ecb743ea386523a10
Aleesha <z5371516@ad.unsw.edu.au>
2022-03-16 22:35:44 +1100

    changed some imports

--- src/server.py
+++ src/server.py
@@ -6,8 +6,8 @@ from flask_cors import CORS
 from src.error import InputError
 from src import config
 
-from src.auth import auth_register_v1, auth_login_v1
-from src.other import clear_v1
+from auth import auth_register_v1, auth_login_v1
+from other import clear_v1
 
 def quit_gracefully(*args):
     '''For coverage'''
