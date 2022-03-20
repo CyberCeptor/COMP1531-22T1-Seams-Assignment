a7628668c65f7525a565d26b7f97b169c621360f
Aleesha <z5371516@ad.unsw.edu.au>
2022-03-21 00:04:06 +1100

    added test for invalid token for logout

--- tests/server_test.py
+++ tests/server_test.py
@@ -4,18 +4,48 @@ import requests
 
 from src import config
 
-def test_logout_works():
+@pytest.fixture(name='clear_and_register')
+def fixture_clear_and_register():
+    """ clears any data stored in data_store and registers a user with the
+    given information
+
+    Arguments: N/A
+
+    Exceptions: N/A
+
+    Return Value: N/A """
+
+    # clear_v1()
+    # auth_register_v1('abc@def.com', 'password', 'first', 'last')
     requests.delete(config.url + 'clear/v1')
 
-    resp0 = requests.post(config.url + 'auth/register/v2', 
-                  json={'email': 'abc@def.com', 'password': 'password',
-                        'name_first': 'first', 'name_last': 'last'})
-    register_data = resp0.json()
-    token = register_data['token']
+    resp = requests.post(config.url + 'auth/register/v2', 
+                         json={'email': 'abc@def.com', 'password': 'password',
+                               'name_first': 'first', 'name_last': 'last'})
+    return resp.json()
+
+def test_logout_works(clear_and_register):
+    token = clear_and_register['token']
 
     resp1 = requests.post(config.url + 'auth/logout/v1',
                          json={'token': token})
     assert resp1.status_code == 200
+
+def test_logout_invalid_token(clear_and_register):
+    # input error: int is passed in as token
+    resp0 = requests.post(config.url + 'auth/logout/v1',
+                         json={'token': 1})
+    assert resp0.status_code == 400
+
+    # access error: non-jwt token str is passed in as token
+    resp1 = requests.post(config.url + 'auth/logout/v1',
+                         json={'token': 'not a valid jwt token str'})
+    assert resp1.status_code == 400
+
+    # input error: bool is passed in as token
+    resp1 = requests.post(config.url + 'auth/logout/v1',
+                         json={'token': True})
+    assert resp1.status_code == 400
 
 # def test_users_return():
 #     requests.delete(config.url + 'clear/v1')
