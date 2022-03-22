19dfd434d37da64e2325e87c98072d51833058c9
Yangjun Yue <z5317840@ad.unsw.edu.au>
2022-03-22 14:48:07 +1100

    save work

--- tests/channels_listall_test.py
+++ tests/channels_listall_test.py
@@ -8,14 +8,14 @@ Description: pytests for channel_listall_v1
 """
 
 import pytest
-
+import requests
 from src.auth import auth_register_v1
 
 from src.other import clear_v1
 from src.error import InputError, AccessError
 
 from src.channels import channels_create_v1, channels_listall_v1
-
+from src import config
 @pytest.fixture(name='clear_and_register_and_create')
 def fixture_clear_and_register_and_create():
     """
@@ -29,10 +29,23 @@ def fixture_clear_and_register_and_create():
     Return Value: N/A
     """
 
-    clear_v1()
-    user1 = auth_register_v1('abc@def.com', 'password', 'first', 'last')
-    chan_id1 = channels_create_v1(1, 'channel_name', True)
-    return [user1['auth_user_id'], chan_id1['channel_id']]
+    # clear_v1()
+    # user1 = auth_register_v1('abc@def.com', 'password', 'first', 'last')
+    # chan_id1 = channels_create_v1(1, 'channel_name', True)
+    # return [user1['auth_user_id'], chan_id1['channel_id']]
+    requests.delete(config.url + 'clear/v1')
+    resp = requests.post(config.url + 'auth/register/v2', 
+                         json={'email': 'abc@def.com', 'password': 'password',
+                               'name_first': 'first', 'name_last': 'last'})
+    user_data = resp.json()
+    token = user_data['token']
+    u_id = user_data['auth_user_id']
+    resp_2 = requests.post(config.url + 'channel/create/v2',
+                            json={'token': token, 'name': 'channel_name',
+                                    'is_public': True})
+    channel_data = resp_2.json()
+    channel_id = channel_data['channel_id']
+    return [token, channel_id, u_id]
 
 def test_channels_listall_invalid_token(clear_and_register_and_create):
     """
