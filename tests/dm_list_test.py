6e09fc0f9161fa1c1988fb765a4b9bb781b88998
Zefan Cao <Z5237177@unsw.edu.au>
2022-03-25 01:58:33 +1100

    small fixtures

--- tests/dm_list_test.py
+++ tests/dm_list_test.py
@@ -1,7 +1,7 @@
 from src.error import InputError, AccessError
 from src.other import clear_v1
 from src.dm import dm_create_v1, dm_list_v1
-from src.auth import auth_register_v1
+from src.auth import auth_register_v2
 def test_dm_list_valid():
     """
     clears any data stored in data_store and registers users with the
@@ -16,9 +16,9 @@ def test_dm_list_valid():
     # Clear the data structure
     clear_v1()
     # Call other functions to create the data and store in data structure
-    user1 = auth_register_v1('wky@gmail.com', '547832', 'wang', 'kaiyan')
-    user2 = auth_register_v1('lmz@gmail.com', '893621', 'li', 'mingzhe')
-    user3 = auth_register_v1('hyf@gmail.com', 'hyf1234', 'huang', 'yifei')
+    user1 = auth_register_v2('wky@gmail.com', '547832', 'wang', 'kaiyan')
+    user2 = auth_register_v2('lmz@gmail.com', '893621', 'li', 'mingzhe')
+    user3 = auth_register_v2('hyf@gmail.com', 'hyf1234', 'huang', 'yifei')
     token1 = user1['token']
     id2 = user2['auth_user_id']
     id3 = user3['auth_user_id']
@@ -33,3 +33,4 @@ def test_dm_list_valid():
     assert len(dm_dict["dms"]) == 2
     assert dm_dict["dms"] == [{'dm_id':1, 'name': 'limingzhe,wangkaiyan'}, 
     {'dm_id':2, 'name': 'huangyifei,wangkaiyan'}]
+clear_v1()
