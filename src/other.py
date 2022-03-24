7dc7e815648ca12fca59c38a2d1adf7ad0cddbb6
Zefan Cao <Z5237177@unsw.edu.au>
2022-03-25 01:51:22 +1100

    add the store[dms].clear()

--- src/other.py
+++ src/other.py
@@ -65,11 +65,10 @@ def check_valid_auth_id(auth_user_id):
         if user['id'] == auth_user_id:
             user_exists = True
             return user
+
     # if the auth_user_id is not found, raise an AccessError
     if user_exists is False:
         raise AccessError('User does not exist in users database')
-
-
 
 def check_valid_channel_id(channel_id):
     """
@@ -85,24 +84,26 @@ def check_valid_channel_id(channel_id):
 
     Return Value: N/A
     """
+    '''Bools are read as int's 0 & 1, so need to check prior. (Not for GET requests)'''
+    '''GET requests are read as a string.'''
+    if type(channel_id) is bool:
+        raise InputError('Invalid channel_id type')
 
-    if isinstance(channel_id, int) is False or type(channel_id) is bool:
-        raise InputError('Channel id is not of a valid type')
+    '''For GET requests.'''
+    channel_id = cast_to_int_get_requests(channel_id, 'channel id')
 
     if channel_id < 1:
         raise InputError('The channel id is not valid (out of bounds)')
 
     store = data_store.get()
-    channel_exists = False
     for channel in store['channels']:
         if channel['channel_id'] == channel_id:
-            channel_exists = True
+            return channel
 
-    # if the auth_user_id is not found, raise an AccessError
-    if channel_exists is False:
-        raise InputError('Channel does not exist in channels database')
+    raise InputError('Channel does not exist in channels database')
 
-def check_user_is_member(auth_user_id, channel_id):
+
+def check_user_is_member(auth_user_id, channel):
     """
     checks if the given user is a member of the given channel by searching
     the stored channel member data
@@ -114,16 +115,56 @@ def check_user_is_member(auth_user_id, channel_id):
     Exceptions: N/A
 
     Return Value:
-        Returns a Boolean depending on if the auth_user_id is found in the
-        channel members data
+        Returns a the channel member data if the auth_user_id is found in the
+        channel. Otherwise returns None
+    """
+
+    for member in channel['all_members']:
+        if member['u_id'] == auth_user_id:
+            return member
+    return None
+
+def check_user_is_owner_member(auth_user_id, channel):
+    for member in channel['owner_members']:
+        if member['u_id'] == auth_user_id:
+            return member
+    return None
+
+def check_user_is_global_owner(auth_user_id):
+    """
+    check the user whether is a global owner with auth user id
+
+    Arguments:
+        auth_user_id (int) - an integer that specifies user id
+
+    Exceptions: N/A
+
+    Return Value: True if the user is a global owner, False otherwise
     """
 
     store = data_store.get()
-    for channel in store['channels']:
-        if channel['channel_id'] == channel_id:
-            for member in channel['all_members']:
-                if member['u_id'] == auth_user_id:
-                    return True
-
+    for user in store['users']:
+        if user['id'] == auth_user_id and user['perm_id'] == 1:
+            return True
     return False
 
+def cast_to_int_get_requests(variable, var_name):
+    """
+    casts the given variable from a get request into an int
+
+    Arguments:
+        variable (any type) - a variable to be cast to an int
+        var_name (str)      - a string used to print out any error messages if
+                              InputError is raised
+    Exceptions:
+        InputError - Raised if the variable can't be turned into an int
+
+    Return Value: Returns the variable casted to an int
+    """
+
+    try:
+        variable = int(variable)
+    except ValueError:
+        raise InputError(description=f'Invalid {var_name}') from InputError
+
+    return variable
